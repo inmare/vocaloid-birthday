9a680fb93ae9fd9a9d9402d77583af5dc7156eb6
inmare <yatsumehole@naver.com>
2025-09-28 22:55:14 +0900

    날짜 선택 component 제작

--- packages/frontend/src/components/App.tsx
+++ packages/frontend/src/components/App.tsx
@@ -1,44 +1,140 @@
 import { useState, useEffect } from "react";
 import clsx from "clsx";
+import dayjs from "dayjs";
 import "@scss/App.scss";
 
 function App() {
+  const [month, setMonth] = useState<number>(-1);
+  const [date, setDate] = useState<number>(-1);
+  const [maxDate, setMaxDate] = useState<number>(-1);
+  const [dateString, setDateString] = useState<string>();
+
   useEffect(() => {
-    // temp
+    const current = dayjs();
+    setMonth(current.month() + 1);
+    setDate(current.date());
+    setMaxDate(current.daysInMonth());
   }, []);
 
-  const [month, setMonth] = useState<number | null>(null);
-  const [date, setDate] = useState<number | null>(null);
+  useEffect(() => {
+    const monthString = String(month);
+    const dateString = date === 0 ? "전체" : String(date) + "일";
+    const str = `${monthString}월 ${dateString}의 곡`;
+    setDateString(str);
+  }, [month, date]);
+
+  const [selectMode, setSelectMode] = useState<"month" | "date">("month");
 
   const monthNumber = Array.from({ length: 12 }, (_, i) => i + 1);
+  const dateNumber = Array.from({ length: 31 }, (_, i) => i + 1);
 
   const [isPickingDate, setIsPickingDate] = useState(!false);
 
-  const handleMonth = () => {
+  const handlePickingType = () => {
     setIsPickingDate(!isPickingDate);
+  };
+
+  const handleMonthClick = (value: number) => {
+    setMonth(value);
+    const maxDate = dayjs()
+      .set("month", value - 1)
+      .daysInMonth();
+    setMaxDate(maxDate);
+    if (date > maxDate) {
+      setDate(maxDate);
+    }
+  };
+
+  const handleDateClick = (value: number) => {
+    if (maxDate === -1 || value > maxDate) {
+      return;
+    }
+
+    setDate(value);
   };
 
   return (
     <>
       <div className="wrapper">
-        <div className="date-picker-wrapper">
-          <button onClick={handleMonth}>날짜 선택</button>
-          <div
-            className={clsx("picker-wrapper", {
-              hide: isPickingDate,
-            })}
-          >
-            <div className="picker month">
-              {monthNumber.map((value) => {
-                return <p>{value}월</p>;
+        <div className="date-picker-section">
+          <div className="date-picker-wrapper">
+            <button onClick={handlePickingType}>날짜 선택</button>
+            <div
+              className={clsx("picker-wrapper", {
+                hide: isPickingDate,
               })}
+            >
+              <div className="picker-type">
+                <div
+                  className={clsx("button", {
+                    active: selectMode === "month",
+                  })}
+                  onClick={() => setSelectMode("month")}
+                >
+                  월
+                </div>
+                <div
+                  className={clsx("button", {
+                    active: selectMode === "date",
+                  })}
+                  onClick={() => setSelectMode("date")}
+                >
+                  일
+                </div>
+              </div>
+
+              <div
+                className={clsx("picker", "month", {
+                  hide: selectMode !== "month",
+                })}
+              >
+                {monthNumber.map((value) => {
+                  return (
+                    <div
+                      className={clsx("button", {
+                        active: month === value,
+                      })}
+                      onClick={() => handleMonthClick(value)}
+                    >
+                      {value}월
+                    </div>
+                  );
+                })}
+              </div>
+              <div
+                className={clsx("picker", "date", {
+                  hide: selectMode !== "date",
+                })}
+              >
+                {dateNumber.map((value) => {
+                  return (
+                    <div
+                      className={clsx("button", {
+                        active: date === value,
+                        deactivate: maxDate !== -1 && value > maxDate,
+                      })}
+                      onClick={() => handleDateClick(value)}
+                    >
+                      {value}일
+                    </div>
+                  );
+                })}
+                <div
+                  className={clsx("button", {
+                    active: date === 0,
+                  })}
+                  onClick={() => handleDateClick(0)}
+                >
+                  전체
+                </div>
+              </div>
             </div>
-            <div className="picker date"></div>
-            <button>선택</button>
           </div>
         </div>
-        <div className="song-display-wrapper">곡 리스트</div>
-        <div className="song-info-wrapper">곡 정보</div>
+        <div className="song-display-section">
+          <p>{dateString}</p>
+        </div>
+        <div className="song-info-section">곡 정보</div>
       </div>
     </>
   );
