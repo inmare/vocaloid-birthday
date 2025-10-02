f7249dfe5d50065ac98f8dcd136f0fbc91215d07
inmare <yatsumehole@naver.com>
2025-10-02 17:41:53 +0900

    불필요한 코드 제거

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,5 @@
 import express, { type Express, Request, Response } from "express";
 import cors from "cors";
-import axios from "axios";
 import dotenv from "dotenv";
 import { connectDatabase, Song, PV } from "@vocaloid-birthday/database";
 import { Op, fn, col, where } from "sequelize";
@@ -19,31 +18,32 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello world!");
 });
 
-function extractVideoId(url: string): string | null {
-  const regex = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
-  const match = url.match(regex);
-  if (match) {
-    const result = match[1] ? match[1] : null;
-    return result;
-  }
-  return null;
-}
-
-app.post("/api/birthday", async (req: Request, res: Response) => {
+app.post("/api/songs", async (req: Request, res: Response) => {
   const { month, date } = req.body;
   const monthString = String(month).padStart(2, "0");
-  const dateString = String(date).padStart(2, "0");
+  const condition = [
+    where(fn("strftime", "%m", col("publishDate")), monthString),
+  ];
+
+  if (date !== 0) {
+    const dateString = String(date).padStart(2, "0");
+    condition.push(where(fn("strftime", "%d", col("publishDate")), dateString));
+  }
 
   try {
     const songList = await Song.findAll({
       where: {
-        [Op.and]: [
-          where(fn("strftime", "%m", col("publishDate")), monthString),
-          where(fn("strftime", "%d", col("publishDate")), dateString),
-        ],
+        [Op.and]: condition,
       },
+      group: ["vocaDBId"], // 중복 제거
+      include: [
+        {
+          model: PV,
+          required: true, // inner join하려면 true로 변경
+        },
+      ],
+      order: [["vocaDBRating", "DESC"]],
     });
-    console.log(songList);
 
     res.status(200).json(songList);
   } catch (error) {
@@ -55,14 +55,10 @@ app.post("/api/birthday", async (req: Request, res: Response) => {
   }
 });
 
-app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기 중!");
-});
-
 const startServer = async () => {
   await connectDatabase({ debug: false });
-  app.listen(port, () => {
-    console.log(`서버가 포트 ${port}에서 시작되었습니다.`);
+  app.listen(app.get("port"), () => {
+    console.log(app.get("port"), "번 포트에서 대기 중!");
   });
 };
 
