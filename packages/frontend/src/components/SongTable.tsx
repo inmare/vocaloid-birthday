8499bf5735ddf83f76a26dcbd12311d4f8ac6ca7
inmare <yatsumehole@naver.com>
2025-10-23 13:37:04 +0900

    노래 정보 표 컴포넌트 생성

--- packages/frontend/src/components/SongTable.tsx
+++ packages/frontend/src/components/SongTable.tsx
@@ -1,26 +1,89 @@
-import { type SongAttributes } from "@vocaloid-birthday/common";
+import {
+  type PVAttributes,
+  type SongAttributes,
+} from "@vocaloid-birthday/common";
+import styled from "styled-components";
 
-export default function SongTable({ song }: { song: SongAttributes }) {
-  const keysToEliminate = ["id", "createdAt", "updatedAt"];
-  const filteredSong = [];
-  for (const [key, value] of Object.entries(song)) {
-    if (keysToEliminate.includes(key)) continue;
-    if (value === null) continue;
-    filteredSong.push([key, value]);
+const MainTable = styled.table`
+  border-collapse: collapse;
+  th {
+    border: 1px solid black;
+    padding: 5px 10px;
   }
 
+  td {
+    border: 1px solid black;
+    padding: 5px 10px;
+  }
+`;
+
+interface SongWithPVs extends SongAttributes {
+  PVs: PVAttributes[];
+}
+
+export default function SongTable({ song }: { song: SongWithPVs }) {
+  const createPVLink = (pvList: PVAttributes[]) => {
+    return (
+      <>
+        {pvList.map((value, index) => {
+          const pvId = value.pvId;
+          const service = value.service;
+          let link: string;
+          if (service === "Youtube") {
+            link = `https://youtu.be/${pvId}`;
+          } else if (service === "NicoNicoDouga") {
+            link = `https://nicovideo.jp/watch/${pvId}`;
+          } else if (service === "BiliBili") {
+            link = `https://www.bilibili.com/video/${pvId}`;
+          } else {
+            link = "#";
+          }
+          return (
+            <a key={index} href={link}>
+              {service}
+            </a>
+          );
+        })}
+      </>
+    );
+  };
+  const vocaDBLink = `https://vocadb.net/s/${song.vocaDBId}`;
   return (
     <>
-      <table>
+      <MainTable>
         <tbody>
-          {filteredSong.map(([key, value]) => (
-            <tr key={key}>
-              <th>{key}</th>
-              <td>{String(value)}</td>
-            </tr>
-          ))}
+          <tr>
+            <th>제목</th>
+            <td>{song.title}</td>
+          </tr>
+          <tr>
+            <th>제목(한국어)</th>
+            <td>{song.titleKr ?? ""}</td>
+          </tr>
+          <tr>
+            <th>작곡가</th>
+            <td>{song.composer}</td>
+          </tr>
+          <tr>
+            <th>작곡가(한국어)</th>
+            <td>{song.composerKr ?? ""}</td>
+          </tr>
+          <tr>
+            <th>업로드 날짜</th>
+            <td>{song.publishDate.toString()}</td>
+          </tr>
+          <tr>
+            <th>VocaDB</th>
+            <td>
+              <a href={vocaDBLink}>{song.vocaDBId}</a>
+            </td>
+          </tr>
+          <tr>
+            <th>PV</th>
+            <td>{createPVLink(song.PVs)}</td>
+          </tr>
         </tbody>
-      </table>
+      </MainTable>
     </>
   );
 }
