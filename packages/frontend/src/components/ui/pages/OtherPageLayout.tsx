6d73706c0fb364c7c321352a26a8435cac495a7d
inmare <yatsumehole@naver.com>
2025-10-29 12:08:19 +0900

    layout에 높이 추가

--- packages/frontend/src/components/ui/pages/OtherPageLayout.tsx
+++ packages/frontend/src/components/ui/pages/OtherPageLayout.tsx
@@ -4,13 +4,15 @@ import { Outlet } from "react-router";
 export default function OtherPageLayout() {
   return (
     <>
-      <div>
+      <div className="grid grid-rows-[auto_1fr] h-dvh">
         <header className="flex flex-row content-center justify-center gap-4 p-3">
           <CustomNavLink to="/">Home</CustomNavLink>
           <CustomNavLink to="/about">About</CustomNavLink>
           <CustomNavLink to="/progress">Progress</CustomNavLink>
         </header>
-        <Outlet />
+        <div className="h-full min-h-0">
+          <Outlet />
+        </div>
       </div>
     </>
   );
