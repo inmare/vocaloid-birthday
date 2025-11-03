fcf2fdd150789071b42d9498f6b65cfe9cb30149
inmare <inmare2000@proton.me>
2025-11-04 00:25:05 +0900

    커스텀 textarea 컴포넌트 추가

---
+++ packages/frontend/src/components/ui/fragments/TextInput.tsx
@@ -0,0 +1,21 @@
+import clsx from "clsx";
+import type { TextareaHTMLAttributes } from "react";
+
+export default function TextInput({
+  className,
+  children,
+  ...rest
+}: TextareaHTMLAttributes<HTMLTextAreaElement>) {
+  return (
+    <textarea
+      spellCheck={false}
+      className={clsx(
+        className,
+        "rounded-lg border-none bg-zinc-50 px-2 py-1 text-zinc-950 outline-none",
+      )}
+      {...rest}
+    >
+      {children}
+    </textarea>
+  );
+}
