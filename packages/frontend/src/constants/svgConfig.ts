b598b93949e83b9693d5b85d1ddfa4435d87b7ff
inmare <inmare2000@proton.me>
2025-11-04 00:27:27 +0900

    설정 추가

---
+++ packages/frontend/src/constants/svgConfig.ts
@@ -0,0 +1,15 @@
+const SvgFactor = 5;
+const SvgSizeX = 106;
+const SvgSizeY = 156;
+const SvgDateFont = "AbrilFatface";
+const SvgCaptionFont = "Pretendard Variable";
+const SvgTextFont = "LINESeed JP, LINESeed KR";
+
+export {
+  SvgCaptionFont,
+  SvgDateFont,
+  SvgFactor,
+  SvgSizeX,
+  SvgSizeY,
+  SvgTextFont,
+};
