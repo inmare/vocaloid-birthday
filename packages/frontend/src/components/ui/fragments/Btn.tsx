2d65c63d27ee5db61957d9e6eadca546b66e3558
inmare <inmare2000@proton.me>
2025-11-04 00:24:39 +0900

    Btn 컴포넌트 추가

---
+++ packages/frontend/src/components/ui/fragments/Btn.tsx
@@ -0,0 +1,20 @@
+import clsx from "clsx";
+import type { ButtonHTMLAttributes } from "react";
+
+export default function Btn({
+  className,
+  children,
+  ...rest
+}: ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      className={clsx(
+        className,
+        "cursor-pointer bg-cyan-900 px-1 text-zinc-50 active:bg-cyan-800",
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
