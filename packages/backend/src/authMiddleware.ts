fdf266b0897bbffdef7f48c72d996a412c1dd80a
inmare <yatsumehole@naver.com>
2025-11-05 15:43:02 +0900

    관리자 용 middleware 추가

---
+++ packages/backend/src/authMiddleware.ts
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers["authorization"];
+
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token === null) {
+    return res.status(401).send({ message: "토큰이 없습니다." });
+  }
+
+  jwt.verify(
+    token as string,
+    process.env.JWT_SECRET as string,
+    (err, payload) => {
+      if (err) {
+        return res.status(403).send({ message: "유효하지 않은 토큰입니다." });
+      }
+
+      if (typeof payload !== "object" || !payload.isAdmin) {
+        return res.status(403).send({ message: "관리자 권한이 없습니다." });
+      }
+
+      next();
+    }
+  );
+};
