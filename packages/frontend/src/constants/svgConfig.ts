17ca52cb4ead9255bfc720185b1d027406bb7466
inmare <yatsumehole@naver.com>
2025-11-07 02:02:22 +0900

    폰트 변경 및 추가

--- packages/frontend/src/constants/svgConfig.ts
+++ packages/frontend/src/constants/svgConfig.ts
@@ -2,8 +2,8 @@ const SvgFactor = 5;
 const SvgSizeX = 106;
 const SvgSizeY = 156;
 const SvgDateFont = "AbrilFatface";
-const SvgCaptionFont = "Pretendard Variable";
-const SvgMainFont = "LINESeed JP, LINESeed KR";
+const SvgCaptionFont = "Pretendard JP Variable, Noto Emoji";
+const SvgMainFont = "Paperlogy, Pretendard JP Variable, Noto Color Emoji";
 const SvgSerifFont = "BookkMyungjo";
 
 export {
