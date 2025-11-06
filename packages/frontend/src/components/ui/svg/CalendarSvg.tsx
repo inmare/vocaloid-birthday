ff8de172c8c6031fd4fac912db1d1ddbb7aae44b
inmare <inmare2000@proton.me>
2025-11-06 17:58:57 +0900

    width, height 설정

--- packages/frontend/src/components/ui/svg/CalendarSvg.tsx
+++ packages/frontend/src/components/ui/svg/CalendarSvg.tsx
@@ -1,9 +1,4 @@
-import {
-  SvgDateFont,
-  SvgFactor,
-  SvgSizeX,
-  SvgSizeY,
-} from "@/constants/svgConfig";
+import { SvgDateFont, SvgSizeX, SvgSizeY } from "@/constants/svgConfig";
 import sampleImage from "@assets/lustorus-sample.jpg";
 import Guideline from "@components/ui/svg/Guideline";
 import { Vec2 } from "@components/utils";
@@ -20,7 +15,7 @@ export default function CalendarSvg({
   date: number;
   accentColor: string;
 }) {
-  const svgSize = new Vec2(SvgSizeX * SvgFactor, SvgSizeY * SvgFactor);
+  const svgSize = new Vec2(SvgSizeX, SvgSizeY);
   const currentDate = dayjs()
     .set("year", 2026)
     .set("month", month - 1)
@@ -30,99 +25,100 @@ export default function CalendarSvg({
   const svgRef = useRef<SVGSVGElement | null>(null);
 
   return (
-    <>
-      <svg
-        ref={svgRef}
-        viewBox={Vec2.toStyle([new Vec2(0, 0), svgSize])}
-        xmlns="http://www.w3.org/2000/svg"
-        className="w-75 shadow-[0_0_10px] shadow-zinc-400"
+    <svg
+      ref={svgRef}
+      viewBox={Vec2.toStyle([new Vec2(0, 0), svgSize])}
+      xmlns="http://www.w3.org/2000/svg"
+      width={`${SvgSizeX}mm`}
+      height={`${SvgSizeY}mm`}
+      className="shadow-[0_0_10px] shadow-zinc-400"
+    >
+      <clipPath id="thumbnail-clip">
+        <rect
+          x={(svgSize.x - 250) / 2}
+          y={105}
+          width={250}
+          height={250}
+          rx={20}
+          ry={20}
+        ></rect>
+      </clipPath>
+      <rect width={svgSize.x} height={svgSize.y} fill="#ffffff" />
+      <Guideline factor={5} svgSize={svgSize} visible={true} />
+      <text
+        textAnchor="middle"
+        fontFamily={SvgDateFont}
+        fill="#000000"
+        fontSize={36}
+        transform={`translate(${svgSize.x / 2}, 60)`}
       >
-        <clipPath id="thumbnail-clip">
-          <rect
-            x={(svgSize.x - 250) / 2}
-            y={105}
-            width={250}
-            height={250}
-            rx={20}
-            ry={20}
-          ></rect>
-        </clipPath>
-        <rect width={svgSize.x} height={svgSize.y} fill="#ffffff" />
-        <Guideline factor={5} svgSize={svgSize} visible={true} />
+        {dateString}
+      </text>
+      <g
+        style={{
+          clipPath: "url(#thumbnail-clip)",
+        }}
+      >
+        <image
+          href={sampleImage}
+          transform={`translate(${
+            svgSize.x / 2 - (1280 * 0.4) / 2
+          }, 80) scale(0.4)`}
+        ></image>
+      </g>
+      <text
+        textAnchor="middle"
+        fontFamily={SvgDateFont}
+        fill={accentColor}
+        fontSize={160}
+        transform={`translate(${svgSize.x / 2}, 450)`}
+      >
+        <tspan>{currentDate.get("date")}</tspan>
+      </text>
+      <text
+        transform={`translate(${svgSize.x / 2}, 500)`}
+        textAnchor="middle"
+        fontFamily="BookkMyungjo"
+      >
+        <tspan>아아, 하늘은 이런 색이었구나</tspan>
+      </text>
+      <line
+        width={2}
+        stroke={accentColor}
+        x1={120}
+        y1={460}
+        x2={svgSize.x - 120}
+        y2={460}
+      ></line>
+      <g transform={`translate(35, 600)`}>
         <text
-          textAnchor="middle"
-          fontFamily={SvgDateFont}
-          fill="#000000"
-          fontSize={36}
-          transform={`translate(${svgSize.x / 2}, 60)`}
+          // transform={`translate(0, 10)`}
+          color="#000000"
+          fontFamily="LINE Seed JP"
+          textAnchor="start"
+          fontSize={30}
         >
-          {dateString}
-        </text>
-        <g
-          style={{
-            clipPath: "url(#thumbnail-clip)",
-          }}
-        >
-          <image
-            href={sampleImage}
-            transform={`translate(${
-              svgSize.x / 2 - (1280 * 0.4) / 2
-            }, 80) scale(0.4)`}
-          ></image>
-        </g>
-        <text
-          textAnchor="middle"
-          fontFamily={SvgDateFont}
-          fill={accentColor}
-          fontSize={160}
-          transform={`translate(${svgSize.x / 2}, 450)`}
-        >
-          <tspan>{currentDate.get("date")}</tspan>
+          <tspan>TAK{/* 和田たけあき */}</tspan>
         </text>
         <text
-          transform={`translate(${svgSize.x / 2}, 500)`}
-          textAnchor="middle"
-          fontFamily="BookkMyungjo"
+          transform={`translate(-5, 50)`}
+          color="#000000"
+          fontFamily="LINE Seed JP"
+          textAnchor="start"
+          fontSize={46}
+          fontWeight={700}
+          letterSpacing={"0em"}
         >
-          <tspan>아아, 하늘은 이런 색이었구나</tspan>
-        </text>
-        <line
-          width={2}
-          stroke={accentColor}
-          x1={120}
-          y1={460}
-          x2={svgSize.x - 120}
-          y2={460}
-        ></line>
-        <g transform={`translate(35, 600)`}>
-          <text
-            // transform={`translate(0, 10)`}
-            color="#000000"
-            fontFamily="LINE Seed JP"
-            textAnchor="start"
-            fontSize={30}
-          >
-            <tspan>TAK{/* 和田たけあき */}</tspan>
-          </text>
-          <text
-            transform={`translate(-5, 50)`}
-            color="#000000"
-            fontFamily="LINE Seed JP"
-            textAnchor="start"
-            fontSize={46}
-            fontWeight={700}
-            letterSpacing={"0em"}
-          >
-            <tspan x={-23} y={0}>
-              「１」{/* トラッシュ・ */}
-            </tspan>
-            <tspan x={0} y={48}>
-              MELON{/* アンド・ */}
-            </tspan>
-            <tspan x={0} y={96}>
-              COOKIE{/* トラッシュ! */}
-            </tspan>
-            {/* {textConfigList.map((value: TextConfig) => {
+          <tspan x={-23} y={0}>
+            「１」{/* トラッシュ・ */}
+          </tspan>
+          <tspan x={0} y={48}>
+            MELON{/* アンド・ */}
+          </tspan>
+          <tspan x={0} y={96}>
+            COOKIE{/* トラッシュ! */}
+          </tspan>
+          {/* {textConfigList.map((value: TextConfig) => {
               const typo = value.typo;
 
               return (
@@ -137,20 +133,19 @@ export default function CalendarSvg({
                 </>
               );
             })} */}
-          </text>
-        </g>
-        <g
-          dangerouslySetInnerHTML={{
-            __html: new QRCode({
-              content: "https://example.com",
-              padding: 1,
-              join: true,
-              container: "none",
-            }).svg(),
-          }}
-          transform={`translate(${100}, 100) scale(0.6)`}
-        ></g>
-      </svg>
-    </>
+        </text>
+      </g>
+      <g
+        dangerouslySetInnerHTML={{
+          __html: new QRCode({
+            content: "https://example.com",
+            padding: 1,
+            join: true,
+            container: "none",
+          }).svg(),
+        }}
+        transform={`translate(${100}, 100) scale(0.6)`}
+      ></g>
+    </svg>
   );
 }
