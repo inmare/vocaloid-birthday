3f9eb6a7720d43b8c6d879d0eccb0da28679658f
inmare <yatsumehole@naver.com>
2025-11-07 12:17:28 +0900

    qrcode 추가

---
+++ packages/frontend/src/components/ui/svg/SvgQrCode.tsx
@@ -0,0 +1,39 @@
+import { Vec2 } from "@/components/utils";
+import QRCode from "qrcode-svg";
+import { useEffect, useState } from "react";
+
+export default function QrCode({
+  month,
+  date,
+}: {
+  month: number;
+  date: number;
+}) {
+  const [qrSvgString, setQrSvgString] = useState<string>("");
+
+  useEffect(() => {
+    const qrUrl = `https://vocalendar.moe/2026/${month}/${date}`;
+    const qrCode = new QRCode({
+      content: qrUrl,
+      padding: 1,
+      join: true,
+      container: "svg",
+    });
+
+    const base64 = btoa(qrCode.svg());
+    const href = `data:image/svg+xml;base64,${base64}`;
+    setQrSvgString(href);
+  }, [month, date]);
+
+  const qrSize = 16;
+  const qrPos = new Vec2(85.5 - qrSize / 2, 11);
+
+  return (
+    <image
+      href={qrSvgString}
+      transform={`${Vec2.toStyle([qrPos])}`}
+      width={qrSize}
+      height={qrSize}
+    />
+  );
+}
