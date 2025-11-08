9634baf1decf714a242847e2fdf4b6d3cab22242
inmare <yatsumehole@naver.com>
2025-11-08 12:01:47 +0900

    전체 텍스트 설정 변경 컴포넌트 추가

---
+++ packages/frontend/src/components/ui/svg/TextAllEditor.tsx
@@ -0,0 +1,43 @@
+import { TextEditContext } from "@components/TextEditContext";
+import CutstomLabel from "@components/ui/fragments/CustomLabel";
+import CustomNumberInput from "@components/ui/fragments/CustomNumberInput";
+import { useContext, type ChangeEvent } from "react";
+
+export default function TextAllEditor({
+  fontSize,
+  lineHeight,
+}: {
+  fontSize?: number;
+  lineHeight?: number;
+}) {
+  const textContext = useContext(TextEditContext);
+
+  const updateFontSize = (event: ChangeEvent<HTMLInputElement>) => {
+    textContext.updateData((draft) => {
+      draft.fontSize = parseFloat(event.target.value);
+    });
+  };
+
+  const updateLineHeight = (event: ChangeEvent<HTMLInputElement>) => {
+    textContext.updateData((draft) => {
+      draft.lineHeight = parseFloat(event.target.value);
+    });
+  };
+
+  return (
+    <div className="col-span-2 grid grid-cols-2 gap-1">
+      <CutstomLabel>FontSize</CutstomLabel>
+      <CutstomLabel>LineHeight</CutstomLabel>
+      <CustomNumberInput
+        defaultValue={fontSize}
+        onChange={updateFontSize}
+        step={1}
+      />
+      <CustomNumberInput
+        defaultValue={lineHeight}
+        onChange={updateLineHeight}
+        step={0.1}
+      />
+    </div>
+  );
+}
