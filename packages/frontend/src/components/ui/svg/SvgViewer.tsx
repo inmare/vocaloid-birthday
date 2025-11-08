75266c4b5126a4b15ae59c89bdff98ea601a360e
inmare <yatsumehole@naver.com>
2025-11-08 12:01:34 +0900

    컴포넌트 및 설정 추가

--- packages/frontend/src/components/ui/svg/SvgViewer.tsx
+++ packages/frontend/src/components/ui/svg/SvgViewer.tsx
@@ -1,7 +1,8 @@
 import api from "@/api";
+import { SvgComposerFontSize, SvgTitleFontSize } from "@/constants/svgConfig";
 import { SvgContext } from "@components/SvgContext";
 import { TextEditContext } from "@components/TextEditContext";
-import type { VisibilityState } from "@components/type";
+import type { ImageConfig, VisibilityState } from "@components/type";
 import Btn from "@components/ui/fragments/Btn";
 import CustomTextarea from "@components/ui/fragments/CustomTextarea";
 import CustomTextInput from "@components/ui/fragments/CustomTextInput";
@@ -11,7 +12,9 @@ import TextViewer from "@components/ui/svg/TextViewer";
 import Colorful from "@uiw/react-color-colorful";
 import clsx from "clsx";
 import dayjs from "dayjs";
-import { useContext, useMemo, useRef, useState, type ChangeEvent } from "react";
+import { useContext, useMemo, useState, type ChangeEvent } from "react";
+import ImageInput from "./ImageInput";
+import TextAllEditor from "./TextAllEditor";
 
 export default function SvgViewer({
   month,
@@ -64,21 +67,36 @@ export default function SvgViewer({
   );
 
   const [paletteVisible, setPaletteVisible] = useState<VisibilityState>("hide");
-  const inputFileRef = useRef<HTMLInputElement | null>(null);
-
-  const onFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    console.log(file);
-  };
 
   const inputClassName =
     "min-w-0 border-none bg-zinc-50 pl-1 text-zinc-950 outline-none";
+
+  const [lyrics, setLyrics] = useState<string>("");
+  const [titleKor, setTitleKor] = useState<string>("");
+  const [composerKor, setComposerKor] = useState<string>("");
+  const [imageBase64, setImageBase64] = useState<string | null>(null);
+  const [imagePos, setImagePos] = useState<ImageConfig>({
+    x: 0,
+    y: 0,
+    scale: 4.5 / 100,
+  });
 
   return (
     <>
       <div className="mx-auto grid max-w-xl p-5">
         <div className="flex scale-100 items-center justify-center">
-          <SvgCalendar month={month} date={date} accentColor={accentColor} />
+          <SvgCalendar
+            {...{
+              month,
+              date,
+              accentColor,
+              lyrics,
+              titleKor,
+              composerKor,
+              imageBase64,
+              imagePos,
+            }}
+          />
         </div>
         {isAdmin && (
           <div className="grid w-full gap-1 py-1">
@@ -138,23 +156,45 @@ export default function SvgViewer({
                   name="image-x"
                   type="number"
                   step={0.1}
+                  value={imagePos.x}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                    setImagePos({
+                      ...imagePos,
+                      x: parseFloat(event.target.value),
+                    });
+                  }}
                 />
                 <input
                   className={inputClassName}
                   name="image-y"
                   type="number"
                   step={0.1}
+                  value={imagePos.y}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                    setImagePos({
+                      ...imagePos,
+                      y: parseFloat(event.target.value),
+                    });
+                  }}
                 />
                 <input
                   className={inputClassName}
                   name="image-scale"
                   type="number"
                   step={0.1}
+                  value={imagePos.scale}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                    setImagePos({
+                      ...imagePos,
+                      scale: parseFloat(event.target.value),
+                    });
+                  }}
                 />
               </div>
               <label htmlFor="">작곡가</label>
               <div className="grid grid-cols-2 gap-4">
                 <TextEditContext.Provider value={composerValue}>
+                  <TextAllEditor fontSize={SvgTitleFontSize} lineHeight={1} />
                   <TextViewer />
                   <TextEditor />
                 </TextEditContext.Provider>
@@ -162,31 +202,37 @@ export default function SvgViewer({
               <label htmlFor="">제목</label>
               <div className="grid grid-cols-2 gap-4">
                 <TextEditContext.Provider value={titleValue}>
+                  <TextAllEditor
+                    fontSize={SvgComposerFontSize}
+                    lineHeight={1}
+                  />
                   <TextViewer />
                   <TextEditor />
                 </TextEditContext.Provider>
               </div>
               <label htmlFor="">가사</label>
-              <CustomTextarea />
-              <label htmlFor="">제목(한국어)</label>
-              <CustomTextarea />
-              <label htmlFor="">작곡가(한국어)</label>
-              <CustomTextarea />
-              <label htmlFor="">이미지</label>
-              <Btn
-                className="self-start rounded-md px-2 py-1"
-                onClick={() => {
-                  if (inputFileRef) inputFileRef.current?.click();
+              <CustomTextarea
+                value={lyrics}
+                onChange={(event: ChangeEvent<HTMLTextAreaElement>) => {
+                  setLyrics(event.target.value);
                 }}
-              >
-                선택하기
-                <input
-                  ref={inputFileRef}
-                  onChange={onFileChange}
-                  type="file"
-                  className="hidden"
-                />
-              </Btn>
+              />
+              <label htmlFor="">제목(한국어)</label>
+              <CustomTextarea
+                value={titleKor}
+                onChange={(event: ChangeEvent<HTMLTextAreaElement>) => {
+                  setTitleKor(event.target.value);
+                }}
+              />
+              <label htmlFor="">작곡가(한국어)</label>
+              <CustomTextarea
+                value={composerKor}
+                onChange={(event: ChangeEvent<HTMLTextAreaElement>) => {
+                  setComposerKor(event.target.value);
+                }}
+              />
+              <label htmlFor="">이미지</label>
+              <ImageInput {...{ setImageBase64 }} />
             </div>
 
             <Btn onClick={sendSvgData}>저장하기</Btn>
