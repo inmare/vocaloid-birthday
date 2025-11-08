6c3424f0823e3bfeae18cc7fc281088ca43e359a
inmare <yatsumehole@naver.com>
2025-11-08 20:09:16 +0900

    컴포넌트 이름 변경

--- packages/frontend/src/components/ui/fragments/CustomInput.tsx
+++ packages/frontend/src/components/ui/fragments/CustomInput.tsx
@@ -1,13 +1,14 @@
 import clsx from "clsx";
 import type { InputHTMLAttributes } from "react";
 
-export default function CustomNumberInput({
+export default function CustomInput({
+  type,
   className,
   ...rest
 }: InputHTMLAttributes<HTMLInputElement>) {
   return (
     <input
-      type="number"
+      type={type ? type : "number"}
       className={clsx(
         className,
         "min-w-0 border-none bg-zinc-50 pl-1 text-zinc-950 outline-none",
