2bb19400f48fbd4c825493e819ed4fc875bdd9e7
inmare <yatsumehole@naver.com>
2025-11-08 11:56:43 +0900

    폰트 크기 설정 추가

--- packages/frontend/src/constants/svgConfig.ts
+++ packages/frontend/src/constants/svgConfig.ts
@@ -1,17 +1,19 @@
-const SvgFactor = 5;
 const SvgSizeX = 106;
 const SvgSizeY = 156;
 const SvgDateFont = "AbrilFatface";
 const SvgCaptionFont = "Pretendard JP Variable, Noto Emoji";
 const SvgMainFont = "Paperlogy, Pretendard JP Variable, Noto Color Emoji";
 const SvgSerifFont = "BookkMyungjo";
+const SvgTitleFontSize = 9;
+const SvgComposerFontSize = 6;
 
 export {
   SvgCaptionFont,
+  SvgComposerFontSize,
   SvgDateFont,
-  SvgFactor,
   SvgMainFont,
   SvgSerifFont,
   SvgSizeX,
   SvgSizeY,
+  SvgTitleFontSize,
 };
