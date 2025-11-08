e768997284af2a62301cf7f23fd148181a4f34c6
inmare <yatsumehole@naver.com>
2025-11-08 12:01:00 +0900

    추가 prop 전달

--- packages/frontend/src/components/ui/svg/SvgThumbnail.tsx
+++ packages/frontend/src/components/ui/svg/SvgThumbnail.tsx
@@ -1,16 +1,43 @@
+import type { ImageConfig } from "@/components/type";
 import { SvgSizeX } from "@/constants/svgConfig";
 import sampleImage from "@assets/no-image.png";
-import { Vec2 } from "@components/utils";
+import { useEffect, useMemo, useRef } from "react";
 
-export default function SvgThumbnail() {
+export default function SvgThumbnail({
+  imageBase64,
+  imagePos,
+}: {
+  imageBase64: string | null;
+  imagePos: ImageConfig;
+}) {
   const width = 80;
   const height = 65;
   const x = SvgSizeX / 2 - width / 2;
   const y = 60 - height / 2;
 
   const clipPathId = "thumbnail";
-  const imageScale = 4.5 / 100;
-  const imagePos = new Vec2(0, 15);
+
+  const imageLink = useMemo(() => {
+    return imageBase64 ? imageBase64 : sampleImage;
+  }, [imageBase64]);
+
+  const imageRef = useRef<SVGImageElement | null>(null);
+
+  useEffect(() => {
+    if (!imageRef.current) return null;
+    if (!imagePos) return { x: 0, y: 0, scale: 1 };
+
+    const imgSvg = imageRef.current;
+    const bbox = imgSvg.getBBox();
+    const x = SvgSizeX / 2 - (bbox.width * imagePos.scale) / 2;
+    const y = 60 - (bbox.height * imagePos.scale) / 2;
+
+    return {
+      x: x + imagePos.x,
+      y: y + imagePos.y,
+      scale: imagePos.scale,
+    };
+  }, [imageRef, imagePos]);
 
   return (
     <>
@@ -21,8 +48,9 @@ export default function SvgThumbnail() {
       <g clipPath={`url(#${clipPathId})`}>
         <rect {...{ x, y, width, height }} opacity={0.5} />
         <image
-          href={sampleImage}
-          transform={`${Vec2.toStyle([imagePos])} scale(${imageScale})`}
+          ref={imageRef}
+          href={imageLink}
+          transform={`translate(${imageRealPos?.x}, ${imageRealPos?.y}) scale(${imageRealPos?.scale})`}
         />
         <rect
           {...{ x, y, width, height }}
