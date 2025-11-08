c870aaefbd0a2ac8875d1127a7d0d7a4484df664
inmare <yatsumehole@naver.com>
2025-11-08 11:57:03 +0900

    기본값 변경

--- packages/frontend/src/components/SvgProvider.tsx
+++ packages/frontend/src/components/SvgProvider.tsx
@@ -1,3 +1,4 @@
+import { SvgComposerFontSize, SvgTitleFontSize } from "@/constants/svgConfig";
 import { useMemo, type ReactNode } from "react";
 import { useImmer } from "use-immer";
 import { SvgContext } from "./SvgContext";
@@ -5,12 +6,12 @@ import type { TextConfig, TextItem } from "./type";
 
 export function SvgProvider({ children }: { children: ReactNode }) {
   const [titleConfig, updateTitleConfig] = useImmer<TextConfig>({
-    fontSize: 12,
+    fontSize: SvgTitleFontSize,
     lineHeight: 1,
     items: [] as TextItem[][],
   });
   const [composerConfig, updateComposerConfig] = useImmer<TextConfig>({
-    fontSize: 10,
+    fontSize: SvgComposerFontSize,
     lineHeight: 1,
     items: [] as TextItem[][],
   });
