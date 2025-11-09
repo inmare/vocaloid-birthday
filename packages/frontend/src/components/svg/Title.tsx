491421ae36c5161e012437a710bf438963f83bbd
inmare <inmare2000@proton.me>
2025-11-10 02:40:26 +0900

    텍스트 크기에 따른 위치 계산으로 수정

--- packages/frontend/src/components/svg/Title.tsx
+++ packages/frontend/src/components/svg/Title.tsx
@@ -10,19 +10,41 @@ export default function Title() {
   const defaultX = SvgDefault.sizeX / 2;
   const defaultY = 117.5;
   const groupRef = useRef<SVGGElement | null>(null);
+  const composerRef = useRef<SVGGElement | null>(null);
+  const titleRef = useRef<SVGGElement | null>(null);
 
   const [groupPos, setGroupPos] = useState<[number, number]>([
     defaultX,
     defaultY,
   ]);
 
+  const [titlePos, setTitlePos] = useState<[number, number]>([
+    0,
+    composer.fontSize,
+  ]);
+
+  useLayoutEffect(() => {
+    if (!titleRef.current || !composerRef.current) return;
+    // 작곡가의 높이를 구한 다음 제목 그룹의 y 위치를 조정
+    const composerGroup = composerRef.current;
+    const composerBbox = composerGroup.getBBox();
+    const titleGroup = titleRef.current;
+    const titleBbox = titleGroup.getBBox();
+    const padding = -0.5;
+    setTitlePos([
+      0,
+      composerBbox.y + composerBbox.height + padding - titleBbox.y,
+    ]);
+  }, [title, composer, defaultX, defaultY]);
+
   useLayoutEffect(() => {
     if (!groupRef.current) return;
     const g = groupRef.current;
     const bbox = g.getBBox();
     const y = defaultY - bbox.height / 2;
+    // const y = defaultY - visualCenterY;
     setGroupPos([defaultX, y]);
-  }, [title, composer, defaultX, defaultY]);
+  }, [titlePos, defaultX, defaultY]);
 
   const createTextItems = (line: TextItem[]) => {
     return line.map((item) => {
@@ -59,7 +81,12 @@ export default function Title() {
       textAnchor="middle"
       transform={`${Vec2.toStyle([new Vec2(...groupPos)])}`}
     >
-      <g fontSize={composer.fontSize} fontWeight={300}>
+      <g
+        fontFamily={SvgDefault.composerFont}
+        fontSize={composer.fontSize}
+        fontWeight={300}
+        ref={composerRef}
+      >
         {createTextLines(
           composer.items,
           composer.fontSize,
@@ -67,9 +94,11 @@ export default function Title() {
         )}
       </g>
       <g
+        fontFamily={SvgDefault.titleFont}
         fontSize={title.fontSize}
-        transform={`translate(0, ${composer.fontSize * composer.lineHeight * composer.items.length + 3})`}
+        transform={`${Vec2.toStyle([new Vec2(...titlePos)])}`}
         fontWeight={700}
+        ref={titleRef}
       >
         {createTextLines(title.items, title.fontSize, title.lineHeight)}
       </g>
