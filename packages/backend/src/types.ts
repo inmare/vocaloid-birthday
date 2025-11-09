4232aec75e97544ea1ae7741a6491f1dc1e99e22
inmare <inmare2000@proton.me>
2025-11-10 03:13:48 +0900

    불필요한 타입 삭제

--- packages/backend/src/types.ts
+++ packages/backend/src/types.ts
@@ -4,13 +4,3 @@ export type AdminData = {
 export type RefreshTokenData = {
   refreshTokens: string[];
 };
-export type CalendarData = {
-  title: string;
-  composer: string;
-  titleKor: string;
-  composerKor: string;
-  calendarDate: Date;
-  lyrics: string;
-  svgConfig: object;
-  songId: number;
-};
