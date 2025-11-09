59777fe6813ab8f1c0894e7a6aa3901045e7b03d
inmare <inmare2000@proton.me>
2025-11-10 02:40:08 +0900

    이미지 로드 시 위치 계산, svg 포맷 원래 형식으로 롤백

--- packages/frontend/src/components/svg/Thumbnail.tsx
+++ packages/frontend/src/components/svg/Thumbnail.tsx
@@ -15,26 +15,11 @@ export default function Thumbnail() {
   const rectX = SvgDefault.sizeX / 2 - rectWidth / 2;
   const rectY = 60 - rectHeight / 2;
 
-  const clipPathId = "thumbnail";
-
   const { fragment } = useContext(SvgContext);
 
   const imageRef = useRef<SVGImageElement | null>(null);
   const [imageLink, setImageLink] = useState<string>(sampleImage);
   const [transformStr, setTransformStr] = useState<string>("");
-
-  useEffect(() => {
-    const image = new Image();
-    image.src = sampleImage;
-    image.onload = () => {
-      const w = image.width;
-      const h = image.height;
-      const scale = SvgDefault.imageScale;
-      const x = SvgDefault.sizeX / 2 - (w * scale) / 2;
-      const y = 60 - (h * scale) / 2;
-      setTransformStr(`translate(${x}, ${y}) scale(${scale})`);
-    };
-  }, []);
 
   useEffect(() => {
     setImageLink(fragment.imageLink || sampleImage);
@@ -43,23 +28,35 @@ export default function Thumbnail() {
   useLayoutEffect(() => {
     if (!imageRef.current) return;
     const imgSvg = imageRef.current;
-    const bbox = imgSvg.getBBox();
-    const x =
-      SvgDefault.sizeX / 2 -
-      (bbox.width * fragment.imageScale) / 2 +
-      fragment.imageX;
-    const y = 60 - (bbox.height * fragment.imageScale) / 2 + fragment.imageY;
-    setTransformStr(`translate(${x}, ${y}) scale(${fragment.imageScale})`);
-  }, [fragment.imageX, fragment.imageY, fragment.imageScale]);
+    const calcImagePos = () => {
+      const bbox = imgSvg.getBBox();
+      const x =
+        SvgDefault.sizeX / 2 -
+        (bbox.width * fragment.imageScale) / 2 +
+        fragment.imageX;
+      const y = 60 - (bbox.height * fragment.imageScale) / 2 + fragment.imageY;
+      setTransformStr(`translate(${x}, ${y}) scale(${fragment.imageScale})`);
+    };
+    // 로드되었을 때도 이미지 위치를 계산하게 이벤트 리스너 추가
+    imgSvg.addEventListener("load", calcImagePos);
+    calcImagePos();
+    return () => {
+      imgSvg.removeEventListener("load", calcImagePos);
+    };
+  }, [imageLink, fragment.imageX, fragment.imageY, fragment.imageScale]);
+
+  const clipPathId = "thumbnail";
+  const blurEffectId = "blur";
 
   return (
     <>
       <clipPath id={clipPathId}>
         <rect x={rectX} y={rectY} width={rectWidth} height={rectHeight} />
       </clipPath>
-
+      <filter id={blurEffectId}>
+        <feGaussianBlur stdDeviation={2} />
+      </filter>
       <g clipPath={`url(#${clipPathId})`}>
-        {/* 배경 placeholder */}
         <rect
           x={rectX}
           y={rectY}
@@ -71,7 +68,7 @@ export default function Thumbnail() {
         <image
           id={SvgDefault.imageId}
           ref={imageRef}
-          href={imageLink}
+          xlinkHref={imageLink}
           transform={transformStr}
         />
         {/* 그림자 */}
@@ -80,11 +77,11 @@ export default function Thumbnail() {
           y={rectY}
           width={rectWidth}
           height={rectHeight}
-          fill="transparent"
+          fill="none"
           stroke="#000000"
           strokeWidth={2}
           opacity={0.75}
-          filter="blur(2px)"
+          filter={`url(#${blurEffectId})`}
         />
       </g>
     </>
