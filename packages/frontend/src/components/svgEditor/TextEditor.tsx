d5ddf468aa0f1d41ea9b52f7a138d78e4c23d144
inmare <inmare2000@proton.me>
2025-11-10 02:43:35 +0900

    input 업데이트 코드 추가

--- packages/frontend/src/components/svgEditor/TextEditor.tsx
+++ packages/frontend/src/components/svgEditor/TextEditor.tsx
@@ -1,6 +1,7 @@
 import { TextEditContext } from "@components/TextEditContext";
 import { Input, Label, Textarea } from "@components/ui";
 import { type ChangeEvent, useContext, useMemo } from "react";
+import type { TextTypo } from "../type";
 
 export default function TextEditor() {
   const { data, updateData } = useContext(TextEditContext);
@@ -31,6 +32,24 @@ export default function TextEditor() {
     });
   };
 
+  const handleValueChange = (
+    event: ChangeEvent<HTMLInputElement>,
+    field: keyof TextTypo,
+  ) => {
+    if (!item) return;
+    const newValue = parseFloat(event.target.value);
+    const id = item.id;
+    updateData((draft) => {
+      for (const line of draft.items) {
+        const idx = line.findIndex((value) => value.id === id);
+        if (idx !== -1) {
+          line[idx].typo[field] = newValue;
+          break;
+        }
+      }
+    });
+  };
+
   return (
     <div>
       <div className="grid grid-cols-[auto_1fr] gap-1">
@@ -41,11 +60,20 @@ export default function TextEditor() {
           className="col-span-2"
         />
         <Label>OffsetX</Label>
-        <Input step={0.1} />
+        <Input
+          onChange={(event) => handleValueChange(event, "offsetX")}
+          step={0.1}
+        />
         <Label>OffsetY</Label>
-        <Input step={0.1} />
+        <Input
+          onChange={(event) => handleValueChange(event, "offsetY")}
+          step={0.1}
+        />
         <Label>Leading</Label>
-        <Input step={0.1} />
+        <Input
+          onChange={(event) => handleValueChange(event, "leading")}
+          step={0.1}
+        />
       </div>
     </div>
   );
