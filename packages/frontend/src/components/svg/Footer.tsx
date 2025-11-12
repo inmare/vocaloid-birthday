6d54d16ac162f27806d7cd04db1a8463dc1af581
inmare <inmare2000@proton.me>
2025-11-13 01:56:04 +0900

    두 줄 이상의 텍스트도 표현하게 변경

--- packages/frontend/src/components/svg/Footer.tsx
+++ packages/frontend/src/components/svg/Footer.tsx
@@ -1,20 +1,44 @@
 import SvgDefault from "@/constants/svgDefaults";
 import { SvgContext } from "@components/SvgContext";
 import { Vec2 } from "@components/utils";
-import { useContext, useMemo } from "react";
+import { useContext, useLayoutEffect, useMemo, useRef, useState } from "react";
 
 export default function Footer() {
-  const footerHeight = 23;
+  const footerHeight = 24;
+  const groupY = 146;
 
   const rectPos = new Vec2(0, SvgDefault.sizeY - footerHeight);
-  const textPos = new Vec2(SvgDefault.sizeX / 2, SvgDefault.sizeY - 14);
+  const [textPos, setTextPos] = useState<[number, number]>([
+    SvgDefault.sizeX / 2,
+    groupY,
+  ]);
 
   const lyricsFontSize = 3.5;
+  const lyricsLineSpacing = 1.3;
 
   const { fragment } = useContext(SvgContext);
+
+  const textGroupRef = useRef<SVGGElement | null>(null);
+
+  useLayoutEffect(() => {
+    if (!textGroupRef.current) return;
+    const g = textGroupRef.current;
+    const bbox = g.getBBox();
+    const y = groupY - bbox.height / 2;
+    setTextPos([SvgDefault.sizeX / 2, y]);
+  }, [fragment.lyrics]);
+
   const captionText = useMemo(() => {
     return `${fragment.titleKor}, ${fragment.composerKor}`;
   }, [fragment.titleKor, fragment.composerKor]);
+
+  const lyricsLines = useMemo(() => {
+    return fragment.lyrics.split("\n");
+  }, [fragment.lyrics]);
+
+  const lyricsLinesLength = useMemo(() => {
+    return lyricsLines.length;
+  }, [lyricsLines]);
 
   return (
     <>
@@ -24,21 +48,30 @@ export default function Footer() {
         transform={`${Vec2.toStyle([rectPos])}`}
         fill={fragment.accentColor}
       />
-      <g transform={`${Vec2.toStyle([textPos])}`}>
-        <text
+      <g
+        ref={textGroupRef}
+        transform={`${Vec2.toStyle([new Vec2(...textPos)])}`}
+      >
+        <g
           textAnchor="middle"
           fontFamily={SvgDefault.serifFont}
           fill="#ffffff"
           fontSize={lyricsFontSize}
         >
-          <tspan>{fragment.lyrics}</tspan>
-        </text>
+          {fragment.lyrics.split("\n").map((line, index) => {
+            return (
+              <text key={index} y={index * lyricsFontSize * lyricsLineSpacing}>
+                {line}
+              </text>
+            );
+          })}
+        </g>
         <text
           textAnchor="middle"
           fontFamily={SvgDefault.captionFont}
           fill="#ffffff"
           fontSize={2.5}
-          dy={lyricsFontSize + 1}
+          dy={lyricsFontSize * lyricsLinesLength * lyricsLineSpacing}
         >
           <tspan>{captionText}</tspan>
         </text>
